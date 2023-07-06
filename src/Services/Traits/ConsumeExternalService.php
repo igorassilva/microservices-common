2b9eae62e6a0f2a8d5da5d87a9ca624cc94f7eb4
Igor Augusto <igor.a.s.silva@gmail.com>
2023-07-06 01:23:29 -0300

    chore: initial commit

---
+++ src/Services/Traits/ConsumeExternalService.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Igorassilva\MicroservicesCommon\Services\Traits;
+
+use Illuminate\Support\Facades\Http;
+
+class ConsumeExternalService
+{
+    public function headers(array $headers = [])
+    {
+        array_push($headers, [
+            'Accept' => 'application/json',
+            'Authorization' => $this->token
+        ]);
+
+        return Http::withHeaders($headers);
+    }
+
+    public function request(
+        string $method,
+        string $endpoint,
+        array $formParams = [],
+        array $headers = []
+    )
+    {
+        return $this->headers($headers)
+                    ->$method($this->url . $endpoint, $formParams);
+    }
+}
